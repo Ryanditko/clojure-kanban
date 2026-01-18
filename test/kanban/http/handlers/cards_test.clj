ac0f8a5901339e4a08d835b2f2283ca14ddc96ca
Ryan Rodrigues Cordeiro <yryurodriguess@gmail.com>
2026-01-18 19:31:41 -0300

    test: add HTTP handlers unit tests

--- test/kanban/http/handlers/cards_test.clj
+++ test/kanban/http/handlers/cards_test.clj
@@ -1,17 +1,30 @@
 (ns kanban.http.handlers.cards-test
-  (:require [clojure.test :refer [deftest is testing use-fixtures]]
-            [ring.mock.request :as mock]
-            [kanban.http.handlers.cards :as handlers]
-            [kanban.domain.cards :as cards]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [kanban.http.handlers.cards :as handlers]))
 
-(use-fixtures :each
-  (fn [f]
-    (cards/clear-all!)
-    (f)))
+(def mock-db nil)
 
-;; TODO: Implement HTTP handlers tests
-;; - Test endpoints: GET, POST, PUT, DELETE
-;; - Test status codes (200, 201, 404, 400)
-;; - Test input validation
-;; - Test JSON response format
+(deftest create-card-handler-invalid-data-test
+  (testing "create-card rejects invalid card data - missing title"
+    (let [response (handlers/create-card {:body-params {:status "todo"}
+                                          :db mock-db})]
+      (is (= 400 (:status response)))
+      (is (= "Validation failed" (get-in response [:body :error]))))))
+
+(deftest get-card-handler-not-found-test
+  (testing "get-card returns 404 for nonexistent card"
+    (let [response (handlers/get-card {:path-params {:id "fake-id"}})]
+      (is (= 404 (:status response)))
+      (is (= "Card not found" (get-in response [:body :error]))))))
+
+(deftest update-card-handler-not-found-test
+  (testing "update-card returns 404 for nonexistent card"
+    (let [response (handlers/update-card {:path-params {:id "fake-id"}
+                                          :body-params {:title "Test"}})]
+      (is (= 404 (:status response))))))
+
+(deftest delete-card-handler-not-found-test
+  (testing "delete-card returns 404 for nonexistent card"
+    (let [response (handlers/delete-card {:path-params {:id "fake-id"}})]
+      (is (= 404 (:status response))))))
 
